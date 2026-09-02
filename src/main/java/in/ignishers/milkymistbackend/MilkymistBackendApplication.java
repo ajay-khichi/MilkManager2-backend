ac77fde010bd31a349a01d3689d8fc9b6a4e88f4
ajay-khichi <ajaysinghkhichi@gmail.com>
2026-09-02 11:56:16 +0530

    initial commit

---
+++ src/main/java/in/ignishers/milkymistbackend/MilkymistBackendApplication.java
@@ -0,0 +1,13 @@
+package in.ignishers.milkymistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkymistBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkymistBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/ignishers/milkymistbackend/MilkymistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ignishers.milkymistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkymistBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
